42a8fe46022ef26e3d15b48b7f6290cd627bb5cd
Fahmi Ramadhan <51454564+fahmi-ramadhan@users.noreply.github.com>
2024-10-01 11:27:48 +0700

    add copilot plugins

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,24 @@
+return {
+    "github/copilot.vim",
+    config = function()
+        -- General copilot settings
+        vim.g.copilot_no_tab_map = true -- Prevents Copilot from taking over the <Tab> key
+        vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true }) -- Set <C-J> to accept Copilot suggestions
+
+        -- Copilot can interfere with some other autocompletion and LSP functions,
+        -- so you can fine-tune how and when it activates, based on your preferences.
+
+        -- Example: enable copilot only for specific filetypes
+        vim.g.copilot_filetypes = {
+            ["*"] = false,  -- disable globally
+            ["javascript"] = true, -- enable only for these
+            ["typescript"] = true,
+            ["lua"] = true,
+            ["python"] = true,
+            ["html"] = true,
+            ["css"] = true,
+            ["scss"] = true,
+        }
+    end,
+}
+
