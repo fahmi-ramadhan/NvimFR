62f4b6ac22a67a5537f7b6f314f032f990a03fce
Fahmi Ramadhan <51454564+fahmi-ramadhan@users.noreply.github.com>
2024-10-08 21:28:15 +0700

    fix: typo in treesitter.lua and missing client param in lsp.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,30 +1,30 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdatel",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = {
-                "lua",
-                "html",
-                "css",
-                "javascript",
-                "typescript",
-                "tsx",
-                "svelte",
-                "go",
-                "json",
-                "python",
-                "java",
-                "rust",
-                "yaml",
-                "cpp",
-            },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-            auto_install = true
-        })
-    end
+		configs.setup({
+			ensure_installed = {
+				"lua",
+				"html",
+				"css",
+				"javascript",
+				"typescript",
+				"tsx",
+				"svelte",
+				"go",
+				"json",
+				"python",
+				"java",
+				"rust",
+				"yaml",
+				"cpp",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+			auto_install = true,
+		})
+	end,
 }
