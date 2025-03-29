93a1f0e1f424fd8a3b6d10b393885296bf51fd67
fahmi-ramadhan <fahmi122004@gmail.com>
2025-03-29 13:52:46 +0700

    chore: update telescope version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             -- tambahin variable builtin
@@ -14,7 +15,8 @@ return {
             -- cari keyword di help page
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
             -- cari semua, show hidden file
-            vim.keymap.set('n', '<leader>fa', "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", {})
+            vim.keymap.set('n', '<leader>fa', "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>",
+                {})
             -- cari keyword di buffer saat ini
             vim.keymap.set('n', '<leader>fz', "<cmd> Telescope current_buffer_fuzzy_find <CR>", {})
         end
