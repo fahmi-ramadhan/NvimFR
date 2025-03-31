6566e99c47797c4225befc2bde3fab04b70fc750
fahmi-ramadhan <fahmi122004@gmail.com>
2025-03-31 21:29:39 +0700

    fix: temporary fix for bufferline and lualine being not transparent issue https://github.com/folke/tokyonight.nvim/issues/703

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,16 +3,25 @@ return {
     lazy = false,
     priority = 1000, -- Ensure it loads first,
     config = function()
-        require("tokyonight").setup{
+        require("tokyonight").setup {
             transparent = true,
             styles = {
                 sidebars = "transparent",
                 floats = "transparent",
             },
+            on_colors = function(c)
+                -- Because lualine broke stuff with the latest commit
+                c.bg_statusline = c.none
+            end,
+            on_highlights = function(hl, c)
+                -- TabLineFill is currently set to black
+                hl.TabLineFill = {
+                    bg = c.none,
+                }
+            end,
         }
         vim.cmd.colorscheme("tokyonight")
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
 }
-
