fee46a585e5a01a48b845d15ebeb17c9414a5b7e
fahmi-ramadhan <fahmi122004@gmail.com>
2025-10-01 11:51:04 +0700

    feat: enable copilot for dart files

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,31 +1,32 @@
 return {
-	"github/copilot.vim",
-	config = function()
-		-- General copilot settings
-		vim.g.copilot_no_tab_map = true -- Prevents Copilot from taking over the <Tab> key
-		vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true }) -- Set <C-J> to accept Copilot suggestions
+    "github/copilot.vim",
+    config = function()
+        -- General copilot settings
+        vim.g.copilot_no_tab_map = true                                                           -- Prevents Copilot from taking over the <Tab> key
+        vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true }) -- Set <C-J> to accept Copilot suggestions
 
-		-- Copilot can interfere with some other autocompletion and LSP functions,
-		-- so you can fine-tune how and when it activates, based on your preferences.
+        -- Copilot can interfere with some other autocompletion and LSP functions,
+        -- so you can fine-tune how and when it activates, based on your preferences.
 
-		-- Example: enable copilot only for specific filetypes
-		vim.g.copilot_filetypes = {
-			["*"] = false, -- disable globally
-			["javascript"] = true, -- enable only for these
-			["typescript"] = true,
-			["typescriptreact"] = true,
-			["javascriptreact"] = true,
-			["json"] = true,
-			["yaml"] = true,
-			["markdown"] = true,
-			["vim"] = true,
-			["lua"] = true,
-			["python"] = true,
-			["html"] = true,
-			["css"] = true,
-			["scss"] = true,
-			["java"] = true,
-			["tex"] = true,
-		}
-	end,
+        -- Example: enable copilot only for specific filetypes
+        vim.g.copilot_filetypes = {
+            ["*"] = false, -- disable globally
+            ["javascript"] = true, -- enable only for these
+            ["typescript"] = true,
+            ["typescriptreact"] = true,
+            ["javascriptreact"] = true,
+            ["json"] = true,
+            ["yaml"] = true,
+            ["markdown"] = true,
+            ["vim"] = true,
+            ["lua"] = true,
+            ["python"] = true,
+            ["html"] = true,
+            ["css"] = true,
+            ["scss"] = true,
+            ["java"] = true,
+            ["tex"] = true,
+            ["dart"] = true,
+        }
+    end,
 }
