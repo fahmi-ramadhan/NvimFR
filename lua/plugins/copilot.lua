ec15c91c8f240976acd93cff77551ac33c898791
fahmi-ramadhan <fahmi122004@gmail.com>
2025-11-24 14:23:42 +0700

    feat: enable copilot for php files

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,7 +2,7 @@ return {
     "github/copilot.vim",
     config = function()
         -- General copilot settings
-        vim.g.copilot_no_tab_map = true                                                           -- Prevents Copilot from taking over the <Tab> key
+        vim.g.copilot_no_tab_map = true                                                                 -- Prevents Copilot from taking over the <Tab> key
         vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true }) -- Set <C-J> to accept Copilot suggestions
 
         -- Copilot can interfere with some other autocompletion and LSP functions,
@@ -10,7 +10,7 @@ return {
 
         -- Example: enable copilot only for specific filetypes
         vim.g.copilot_filetypes = {
-            ["*"] = false, -- disable globally
+            ["*"] = false,         -- disable globally
             ["javascript"] = true, -- enable only for these
             ["typescript"] = true,
             ["typescriptreact"] = true,
@@ -27,6 +27,7 @@ return {
             ["java"] = true,
             ["tex"] = true,
             ["dart"] = true,
+            ["php"] = true,
         }
     end,
 }
